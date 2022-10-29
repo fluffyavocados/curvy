86dcbf083878cdd0cb699c8f9f4a915b9f832218
Studemy <studemy@Emys-MacBook.local>
2022-10-29 11:56:07 +0200

    Setup pixi.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 import './style.css'
-import { setupCounter } from './counter.js'
+import { Application, Assets, Sprite } from 'pixi.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
+const app = new Application();
 
-setupCounter(document.querySelector('#counter'))
+document.body.appendChild(app.view);
+
+const texture = await Assets.load('bunny.png');
+
+const bunny = new Sprite(texture);
+bunny.x = app.renderer.width / 2;
+bunny.y = app.renderer.height / 2;
+bunny.anchor.x = 0.5;
+bunny.anchor.y = 0.5;
+
+app.stage.addChild(bunny);
+
+app.ticker.add(() => {
+  bunny.y -= 1;
+  bunny.x -= 1;
+  bunny.rotation += 1;
+});
