02963960f24818264ef88272afa8044ec9f57734
Studemy <studemy@Emys-MacBook.local>
2022-10-30 16:49:57 +0100

    Finish Game

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 import "./style.css";
 import { Application, Assets, Sprite, Graphics } from "pixi.js";
 
+const PLAYER_SPEED = 3;
+const POOP_SPEED = 1;
+
+const keyboard = {
+  w: false,
+  a: false,
+  s: false,
+  d: false,
+};
+
+document.addEventListener("keydown", (e) => {
+  if (keyboard[e.key] != undefined) {
+    keyboard[e.key] = true;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (keyboard[e.key] != undefined) {
+    keyboard[e.key] = false;
+  }
+});
+
 const app = new Application();
 document.body.appendChild(app.view as HTMLCanvasElement);
 
@@ -16,8 +38,8 @@ player.x = app.renderer.width / 2;
 player.y = app.renderer.height / 2;
 player.anchor.x = 0.5;
 player.anchor.y = 0.5;
-player.height = 128;
-player.width = 128;
+player.height = 64;
+player.width = 64;
 
 app.stage.addChild(player);
 
@@ -26,9 +48,12 @@ const directionXs: number[] = [];
 const directionYs: number[] = [];
 
 function spawnPoop() {
+  const radius =
+    (app.renderer.width ** 2 + app.renderer.height ** 2) ** 0.5 / 2;
+  const angle = Math.random() * 2 * Math.PI;
   const poop = new Sprite(poopTexture);
-  poop.x = Math.random() * app.renderer.width;
-  poop.y = 0;
+  poop.x = Math.cos(angle) * radius + app.renderer.width / 2;
+  poop.y = Math.sin(angle) * radius + app.renderer.height / 2;
   poop.anchor.x = 0.5;
   poop.anchor.y = 0.5;
   poop.height = 64;
@@ -42,19 +67,51 @@ function spawnPoop() {
   directionYs.push(directionY / (directionY ** 2 + directionX ** 2) ** 0.5);
 }
 
+const startTime = Date.now();
+
 app.ticker.add(() => {
-  player.y -= 1;
-  player.x -= 1;
+  const currentTime = Date.now();
+  const time = currentTime - startTime;
+  if (keyboard.w) {
+    player.y -= PLAYER_SPEED;
+  }
+  if (keyboard.s) {
+    player.y += PLAYER_SPEED;
+  }
+  if (keyboard.d) {
+    player.x += PLAYER_SPEED;
+  }
+  if (keyboard.a) {
+    player.x -= PLAYER_SPEED;
+  }
   player.rotation += 0.05;
 
-  if (Math.random() < 0.05) {
+  if (Math.random() < 0.03) {
     spawnPoop();
   }
   for (let i = 0; i < poops.length; i++) {
     const poop = poops[i];
     const directionX = directionXs[i];
     const directionY = directionYs[i];
-    poop.x += directionX;
-    poop.y += directionY;
+    const multiplier = 1 + time / 60000;
+    poop.x += directionX * POOP_SPEED * POOP_SPEED * multiplier;
+    poop.y += directionY * POOP_SPEED * POOP_SPEED * multiplier;
+
+    const playerDirX = poop.x - player.x;
+    const playerDirY = poop.y - player.y;
+    const length = Math.sqrt(playerDirX ** 2 + playerDirY ** 2);
+    if (length < 48) {
+      document.body.innerHTML = "";
+      document.location.reload();
+    }
   }
+});
+
+const timerElement = document.querySelector(".timer")!;
+app.ticker.add(() => {
+  const currentTime = Date.now();
+
+  const time = currentTime - startTime;
+
+  timerElement.innerHTML = (time / 1000).toFixed(0) + " s";
 });
